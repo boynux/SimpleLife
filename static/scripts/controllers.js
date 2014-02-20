af7639f13e849a441bcef4a588168ceaa2dcdf38
Boynux <boynux@gmail.com>
2014-02-20 19:45:34 +0000

    Some animations added.
    using collie library some animations added.

--- static/scripts/controllers.js
+++ static/scripts/controllers.js
@@ -71,6 +71,183 @@ simpleLifeControllers.controller ('SigninCtrl', ['$rootScope', '$location', '$ht
         $http.get ('/pictures').success (function (info) {
             console.log (info);
             $scope.albums = info;
+
+            var itemColors = ['#74ff00', '#88ff00', '#9dff00', '#b2ff00', '#c7ff00', '#b2ff00', '#9dff00', '#88ff00'];
+            var itemAnimations = [];
+
+            var clientSize = {
+                width : document.body.clientWidth - 300,
+                height : document.body.clientHeight
+            };
+
+            var itemSize = 100;
+
+            var layer = new collie.Layer({
+                width: clientSize.width,
+                height: clientSize.height
+            });
+
+            var pictures = {};
+            var items = [];
+            var itemCount = 0;
+
+            angular.forEach ($scope.albums, function (album) {
+                angular.forEach (album.data, function (photo) {
+                    pictures[photo.id] = photo.picture;
+                });
+            });
+
+            collie.ImageManager.add(pictures);
+
+            angular.forEach (pictures, function (link, id) {
+                item = new collie.DisplayObject({
+                    x: clientSize.width / 2,
+                    y: clientSize.height / 2,
+                    width: itemSize,
+                    height: itemSize,
+                    // velocityRotate: 50,
+                    backgroundImage: id,
+                    backgroundColor: '#FFFFFF'
+                }).addTo(layer);
+
+                items.push(item);
+            });
+
+            var layoutFunctions = [layoutHorizontal, layoutRectangle, layoutCircle];
+            var layoutSelectedIndex = -1;
+
+            collie.Timer.repeat(function(oEvent){
+                layoutSelectedIndex = (++layoutSelectedIndex) % layoutFunctions.length;
+                layoutFunctions[layoutSelectedIndex]();
+            }, 3000);
+
+            itemCount = items.length;
+
+            function layoutHorizontal(){
+                var offsetX = - (itemCount / 2) * itemSize;
+                var offsetY = - itemSize / 2;
+
+                arrangeItems(function(i){
+                    return {
+                        x: offsetX + i*itemSize,
+                        y: offsetY,
+                        width: itemSize,
+                        height: itemSize,
+                        angle: 0,
+                        originX: "center",
+                        originY: "center"
+                    }
+                }, function(frame, idx, info){
+                    if (info.width > 0.4){
+                        info.width -= 0.4;
+                        info.height -= 0.4;
+                    }
+                });
+            }
+
+            function layoutRectangle() {
+                var countX = Math.ceil(Math.sqrt(itemCount));
+                var countY = Math.ceil(itemCount/countX);
+                var offsetX = - (countX/2)*itemSize;
+                var offsetY = - (countY/2)*itemSize;
+
+                arrangeItems(function(i){
+                    return {
+                        x: offsetX + (i%countX)*itemSize,
+                        y: offsetY + parseInt(i/countX)*itemSize,
+                        width: itemSize,
+                        height: itemSize,
+                        angle: 0,
+                        originX: "left",
+                        originY: "top",
+                    }
+                }, function(frame, idx, info){
+                    // info.backgroundColor = itemColors[(frame+idx)%itemColors.length];
+                })
+            }
+
+            function layoutCircle(){
+                var radius = itemSize * 2;
+                var degStart = 90;
+                var degUnit = Math.round(360 / itemCount);
+
+                arrangeItems(function(i){
+                    var deg = degStart - degUnit * i;
+                    var rad = collie.util.toRad(deg);
+
+                    return {
+                        x: radius * Math.cos(rad),
+                    y: radius * Math.sin(rad),
+                    width: itemSize,
+                    height: itemSize,
+                    angle: deg,
+                    originX: "left",
+                    originY: "top"
+                    }
+                }, function(frame, idx, info){
+                    info.angle++;
+                });
+            }
+
+            function arrangeItems(getTransitionInfo, updateRepeatInfo){
+                var centerX = clientSize.width / 2;
+                var centerY = clientSize.height / 2;
+                var from, to;
+                var aFrom, aTo, set, effects;
+                var item;
+
+                while (itemAnimations.length)
+                    itemAnimations.pop().stop();
+
+                for (var i = 0; i < itemCount; i++){
+                    var item = items[i];
+                    from = item.get();
+                    to = getTransitionInfo(i);
+                    aFrom = [];
+                    aTo = [];
+                    set = [];
+                    effects = [];
+
+                    if (typeof to.x != 'undefined') to.x += centerX;
+                    if (typeof to.y != 'undefined') to.y += centerY;
+
+                    for (var key in to){
+                        if (key == "originX" || key == "originY") {
+                            continue;
+                        }
+
+                        from[key] = Math.round(from[key]);
+                        aFrom.push(from[key]);
+                        aTo.push(to[key]);
+                        set.push(key);
+                        effects.push(collie.Effect.easeOutSine);
+                    }
+
+                    var repeat = (function(i, to, item) {
+                        var params = {i:i, to:to, item:item};
+                        return function (animationParams) {
+                            updateRepeatInfo(animationParams.frame, params.i, params.to);
+                            params.item.set(params.to);
+                        }
+                    }) (i, to, item);
+
+                    itemAnimations.push(
+                        collie.Timer.queue().
+                        delay(function(){}, i * 8).
+                        transition(item, 600, {
+                            from:aFrom,
+                            to:aTo,
+                            set:set,
+                            effect: effects
+                        }).
+                        repeat(repeat, 5)
+                    );
+                }
+            }
+
+            collie.Renderer.addLayer(layer);
+            collie.Renderer.load(document.getElementById("container"));
+            collie.Renderer.start();
         }).error (function (reason) {
             !!reason && console.log (reason);
             $http.post ("/renew_token", {redirect_url: $location.absUrl ()})
